eab5a6f4bff1b70ef1bb2f4005e4a67dd09cbcf1
Jeff Liu <49432090+JeffLiusGitHub@users.noreply.github.com>
2022-10-07 21:40:04 +0930

    change contact animation style

--- src/page/Contact.tsx
+++ src/page/Contact.tsx
@@ -24,6 +24,7 @@ const Contract = ({ textEnter, textLeave }: Props) => {
             onMouseLeave={textLeave}
             style={{
               opacity: isContentInView ? 1 : 0,
+              transform: isContentInView ? 'none' : 'translateY(200px)',
               transition: 'all 1.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
             }}
           >
@@ -34,6 +35,7 @@ const Contract = ({ textEnter, textLeave }: Props) => {
             onMouseLeave={textLeave}
             style={{
               opacity: isContentInView ? 1 : 0,
+              transform: isContentInView ? 'none' : 'translateY(200px)',
               transition: 'all 1.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
             }}
           >
