d953873d71e35083ed51ce41d6c413664beb9375
Zexi Liu <49432090+JeffLiusGitHub@users.noreply.github.com>
2026-02-25 21:21:15 +1100

    Updated App.tsx to remove framer-motion and use optimized cursor.

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,14 @@ import Home from './page/Home';
 import Contract from './page/Contact';
 import TimeLine from './page/TimeLine';
 import Works from './page/Works';
-import { motion } from 'framer-motion';
 import useMouse from './components/useMouse';
 
 function App() {
-  const { textEnter, textLeave, variants, cursorVariant } = useMouse();
+  const { cursorRef, textEnter, textLeave } = useMouse();
 
   return (
-    <div
-      style={{
-        overflowX: 'hidden',
-      }}
-    >
-      <motion.div
-        className="cursor"
-        variants={variants}
-        animate={cursorVariant}
-      />
+    <div style={{ overflowX: 'hidden' }} >
+      <div ref={cursorRef} className="cursor" />
       <Navbar />
       <Home textEnter={textEnter} textLeave={textLeave} />
       <About textEnter={textEnter} textLeave={textLeave} />
